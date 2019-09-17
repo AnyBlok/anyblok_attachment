a81d9b5726753c61a186c24777b0527e1a0f0a3d
jssuzanne <js.suzanne@gmail.com>
2019-09-17 17:52:14 +0200

    Refactored low level to use pytest

--- anyblok_attachment/bloks/attachment/tests/test_historized.py
+++ anyblok_attachment/bloks/attachment/tests/test_historized.py
@@ -1,40 +1,42 @@
 # This file is a part of the AnyBlok / Attachment api project
 #
 #    Copyright (C) 2017 Jean-Sebastien SUZANNE <jssuzanne@anybox.fr>
+#    Copyright (C) 2019 Jean-Sebastien SUZANNE <js.suzanne@gmail.com>
 #
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
 # obtain one at http://mozilla.org/MPL/2.0/.
-from anyblok.tests.testcase import BlokTestCase
+import pytest
 from anyblok_mixins.mixins.exceptions import (
     ForbidUpdateException, ForbidDeleteException)
 from os import urandom
 
 
-class TestHistorized(BlokTestCase):
+@pytest.mark.usefixtures('rollback_registry')
+class TestHistorized:
 
-    def test_query_only_history(self):
-        document = self.registry.Attachment.Document.Latest.insert()
-        history = self.registry.Attachment.Document.History.insert(
+    def test_query_only_history(self, rollback_registry):
+        registry = rollback_registry
+        document = registry.Attachment.Document.Latest.insert()
+        history = registry.Attachment.Document.History.insert(
             uuid=document.uuid, version_number=100000)
-        self.assertIs(
-            self.registry.Attachment.Document.History.query().filter_by(
-                uuid=document.uuid).one(),
-            history
-        )
+        assert registry.Attachment.Document.History.query().filter_by(
+            uuid=document.uuid).one() is history
 
-    def test_update(self):
+    def test_update(self, rollback_registry):
+        registry = rollback_registry
         file_ = urandom(100)
-        document = self.registry.Attachment.Document.insert(file=file_)
+        document = registry.Attachment.Document.insert(file=file_)
         document.historize_a_copy()
-        with self.assertRaises(ForbidUpdateException):
+        with pytest.raises(ForbidUpdateException):
             document.previous_version.data = {'other': 'data'}
-            self.registry.flush()
+            registry.flush()
 
-    def test_delete(self):
+    def test_delete(self, rollback_registry):
+        registry = rollback_registry
         file_ = urandom(100)
-        document = self.registry.Attachment.Document.insert(file=file_)
+        document = registry.Attachment.Document.insert(file=file_)
         document.historize_a_copy()
-        with self.assertRaises(ForbidDeleteException):
+        with pytest.raises(ForbidDeleteException):
             document.previous_version.delete()
-            self.registry.flush()
+            registry.flush()
